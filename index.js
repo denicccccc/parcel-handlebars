7fb24872212251b5c05a485ce5eac2bd31a86035
denicccccc <goncharov.den0410@gmail.com>
2025-11-25 08:28:48 +0100

    js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { products } from "./data.js";
 
-// Завантажуємо шаблон (fetch працює для локальних файлів у браузері)
-fetch("./src/template.hbs")
-  .then(response => response.text())
-  .then(templateSource => {
     const template = Handlebars.compile(`
       {{#each this}}
         <li class="product-item">
@@ -17,4 +13,3 @@ fetch("./src/template.hbs")
     const markup = template(products);
 
     document.querySelector(".product-list").innerHTML = markup;
-  });
